1a410e68c6814d7376f3e4fdc1636a445734c59e
fahadmak <fahad.makabugo@andela.com>
2019-03-25 18:02:48 +0300

    feat(DetailActivity): Launch DetailActivity
    [Delivers #155820624]

--- app/src/main/java/com/example/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -1,14 +1,41 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String TAG = "DetailActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+
+        Bundle extras = intent.getExtras();
+
+        String username = extras.getString(MainActivity.USER_NAME);
+
+        TextView usernameText = findViewById(R.id.username_info);
+        usernameText.setText(username);
+
+        TextView nameText = findViewById(R.id.full_name);
+        nameText.setText(username);
+
+        int imageResource = extras.getInt(MainActivity.IMAGE);
+
+        ImageView imageView = findViewById(R.id.detail_image);
+        imageView.setImageResource(imageResource);
+
     }
 }
